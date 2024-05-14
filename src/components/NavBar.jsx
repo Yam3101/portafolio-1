c06227fea20323855b2fd3f03180c81bbf6d1895
Yam3101 <angelmay_810@hotmail.com>
2024-05-14 16:47:05 -0500

    changes for visual logos fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,13 +5,13 @@ export default function NavBar() {
     <>
         <nav className="container-nav-info">
             <div className="container-logo">
-                <img width={50} src="/public/faviconYam.svg" alt="logo" />
+                <img width={50} src="/faviconYam.svg" alt="logo" />
                 <h1 className="nav-title">YAM&apos;S PROJECTS</h1>
                 <h1 className="nav-title-2">YAM&apos;S</h1>
             </div>
             <div className="contact-logos">
-                <a href="https://github.com/Yam3101"><img width={40} src="/public/github.svg" alt="GitHub" className="git-logo"/></a>
-                <a href=""><img width={40} src="/public/discord.svg" alt="Discord" className="disc-logo"/></a>
+                <a href="https://github.com/Yam3101"><img width={40} src="/github.svg" alt="GitHub" className="git-logo"/></a>
+                <a href=""><img width={40} src="/discord.svg" alt="Discord" className="disc-logo"/></a>
                 <a href=""><img width={40} src="/twitter.svg" alt="Twitter" className="twt-logo"/></a>
             </div>
         </nav>
