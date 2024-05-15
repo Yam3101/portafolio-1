e5682465d7f29d011113466032f29a1590df217e
Yam3101 <angelmay_810@hotmail.com>
2024-05-14 23:41:21 -0500

    changes for responsive UI dev tools

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -9,21 +9,49 @@ function Tools() {
                 <h4 className="title-tool"><span className="htmlTxt">HTML5</span></h4>
             </div>
             <div className="experience-cont">
-                <h5 className="experience-text">Experience:</h5>
-                <div className="experience-bar-html"><span className="bar-text">HIGH KNOWLEDGE</span></div>
+                <h5 className="experience-text">Experience: 2 years</h5>
+                <div className="experience-bar-high"><span className="bar-text"></span></div>
             </div>
         </div>
         <div className="card-tool">
-            <h4 className="title-tool">CSS3</h4>
+            <div className="container-head-card">
+                <img width={50} src="/cssLogo.png" alt="cssLogo" />
+                <h4 className="title-tool"><span className="htmlTxt">CSS3</span></h4>
+            </div>
+            <div className="experience-cont">
+                <h5 className="experience-text">Experience: 2 years</h5>
+                <div className="experience-bar-too-high"><span className="bar-text"></span></div>
+            </div>
         </div>
         <div className="card-tool">
-            <h4 className="title-tool">JavaScript</h4>
+            <div className="container-head-card">
+                <img width={50} src="/javascriptLogo.png" alt="jsLogo" />
+                <h4 className="title-tool"><span className="htmlTxt">JavaScript</span></h4>
+            </div>
+            <div className="experience-cont">
+                <h5 className="experience-text">Experience: 1 year</h5>
+                <div className="experience-bar-mid"><span className="bar-text"></span></div>
+            </div>
         </div>
         <div className="card-tool">
-            <h4 className="title-tool">Java</h4>
+            <div className="container-head-card">
+                <img width={50} src="/phpLogo.png" alt="phpLogo" />
+                <h4 className="title-tool"><span className="htmlTxt">PHP</span></h4>
+            </div>
+            <div className="experience-cont">
+                <h5 className="experience-text">Experience: &#x3C; 1 year</h5>
+                <div className="experience-bar-low"><span className="bar-text"></span></div>
+            </div>
         </div>
         <div className="card-tool">
-            <h4 className="title-tool">PHP</h4>
+            <div className="container-head-card">
+                <img width={50} src="/javaLogo.png" alt="javaLogo" />
+                <h4 className="title-tool"><span className="htmlTxt">Java</span></h4>
+            </div>
+            <div className="experience-cont">
+                <h5 className="experience-text">Experience: 1 year</h5>
+                <div className="experience-bar-mid"><span className="bar-text"></span></div>
+            </div>
         </div>
     </div>
   )
