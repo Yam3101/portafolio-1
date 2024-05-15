38934fda7d5353e320993c3afd1d5ca501564b69
Yam3101 <angelmay_810@hotmail.com>
2024-05-14 22:44:03 -0500

    changes for dev tools UI

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -4,7 +4,14 @@ function Tools() {
   return (
     <div className="container-tools">
         <div className="card-tool">
-            <h4 className="title-tool"><span className="htmlTxt">HTML5</span></h4>
+            <div className="container-head-card">
+                <img width={50} src="/htmlLogo.png" alt="html" />
+                <h4 className="title-tool"><span className="htmlTxt">HTML5</span></h4>
+            </div>
+            <div className="experience-cont">
+                <h5 className="experience-text">Experience:</h5>
+                <div className="experience-bar-html"><span className="bar-text">HIGH KNOWLEDGE</span></div>
+            </div>
         </div>
         <div className="card-tool">
             <h4 className="title-tool">CSS3</h4>
