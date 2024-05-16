ec136fe822c645e7674e1f88ba735abd11b9b026
Yam3101 <angelmay_810@hotmail.com>
2024-05-16 09:04:34 -0500

    animations added to website

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -3,13 +3,13 @@ import "./components.css"
 function Tools() {
   return (
     <div className="container-tools">
-        <img src="/vscLogo.png" alt="vscLogo" />
-        <img src="/gitLogo.png" alt="" />
-        <img src="/htmlLogo.png" alt="htmlLogo" />
-        <img src="/cssLogo.png" alt="cssLogo" />
-        <img src="/javascriptLogo.png" alt="jsLogo" />
-        <img src="/viteLogo.png" alt="viteLogo" />
-        <img src="/reactLogo.png" alt="reactLogo" />
+        <img id="tool-vsc" className="logos-tools" src="/vscLogo.png" alt="vscLogo" />
+        <img id="tool-git" className="logos-tools" src="/gitLogo.png" alt="" />
+        <img id="tool-html" className="logos-tools" src="/htmlLogo.png" alt="htmlLogo" />
+        <img id="tool-css" className="logos-tools" src="/cssLogo.png" alt="cssLogo" />
+        <img id="tool-js" className="logos-tools" src="/javascriptLogo.png" alt="jsLogo" />
+        <img id="tool-vite" className="logos-tools" src="/viteLogo.png" alt="viteLogo" />
+        <img id="tool-jsx" className="logos-tools" src="/reactLogo.png" alt="reactLogo" />
     </div>
   )
 }
