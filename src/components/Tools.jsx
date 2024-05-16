78f68ab870fa16a28436ad9f7219e4921bf333f0
Yam3101 <angelmay_810@hotmail.com>
2024-05-16 11:23:20 -0500

    removed unneeded CSS styles 1.5

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -3,13 +3,15 @@ import "./components.css"
 function Tools() {
   return (
     <div className="container-tools">
-        <img id="tool-vsc" className="logos-tools" src="/vscLogo.png" alt="vscLogo" />
-        <img id="tool-git" className="logos-tools" src="/gitLogo.png" alt="gitLogo" />
-        <img id="tool-html" className="logos-tools" src="/htmlLogo.png" alt="htmlLogo" />
-        <img id="tool-css" className="logos-tools" src="/cssLogo.png" alt="cssLogo" />
-        <img id="tool-js" className="logos-tools" src="/javascriptLogo.png" alt="jsLogo" />
-        <img id="tool-vite" className="logos-tools" src="/viteLogo.png" alt="viteLogo" />
-        <img id="tool-jsx" className="logos-tools" src="/reactLogo.png" alt="reactLogo" />
+        <img className="logos-tools" src="/vscLogo.png" alt="vscLogo" />
+        <img className="logos-tools" src="/gitLogo.png" alt="gitLogo" />
+        <img className="logos-tools" src="/htmlLogo.png" alt="htmlLogo" />
+        <img className="logos-tools" src="/cssLogo.png" alt="cssLogo" />
+        <img className="logos-tools" src="/javascriptLogo.png" alt="jsLogo" />
+        <img className="logos-tools" src="/viteLogo.png" alt="viteLogo" />
+        <img className="logos-tools" src="/reactLogo.png" alt="reactLogo" />
+        <img className="logos-tools" src="/canvaLogo.png" alt="cssLogo" />
+        <img className="logos-tools" src="/vercelLogo.png" alt="cssLogo" />
     </div>
   )
 }
