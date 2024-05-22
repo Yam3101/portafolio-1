b2fbb6db778af9f40e024b38675746575e508ca8
Yam3101 <angelmay_810@hotmail.com>
2024-05-22 07:15:07 -0500

    changed CSS to Tailwind trying to get fonts and removing CSS

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,9 @@ function App() {
             <h1 className='text-blue-600 font-bold text-5xl font-reem'>FRONT-END DEVELOPER STUDENT</h1>
           </div>
         </div>
-        <div className='container-info'>
-        <div className='bg-blue-600 h-2 back-color-animated mt-2'></div>
-          <div className='text-about-me p-5 text-center font-dmsans'>
+        <div className='grid place-content-center'>
+          <div className='bg-blue-600 h-2 back-color-animated mt-2'></div>
+          <div className='textWidth p-5 text-center font-DMsans'>
             My name is Angel Yam, I&apos;m a Front-End Engineering Student, I have knowledge on UI development, 
             Front-End Tools, B1 English Level and many experience on work team projects.  
           </div>
