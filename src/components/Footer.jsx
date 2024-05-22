ceff037730c8a9a74594fffad70175d5870c75a4
Yam3101 <angelmay_810@hotmail.com>
2024-05-22 08:13:29 -0500

    renderized images

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,11 @@ import "./components.css"
 function Footer() {
 
   return (
-    <footer className="footer-web">
+    <footer className="bg-black p-7 text-sm font-DMsans mt-16 flex justify-between">
         Portfolio made by Yam
-    <p>Website made in React + Vite</p>
-    <div className="img-dev">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae harum odio nemo minus beatae fugiat perferendis nobis, et modi vitae quisquam ullam numquam libero sapiente! Perspiciatis veniam illo dolore adipisci!
-    </div>
+        <p>This is a simple portfolio made in React.Js, if you want to know about me or my projects you can
+          contact me in any moment
+        </p>
     </footer>
   )
 }
